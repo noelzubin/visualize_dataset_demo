42049688283e70c0786504093a1d2d13b714f146
noelzubin <noelzubinvictor@gmail.com>
2017-02-19 16:07:05 +0530

    changed test_train splitting to sampling, added python3 support

--- data_visualization.py
+++ data_visualization.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
-from sklearn.cross_validation import train_test_split
 from sklearn.metrics import accuracy_score
 
 # visulaize the important characteristics of the dataset
@@ -23,7 +22,10 @@ dataframe_all = dataframe_all[counter_without_nan.keys()]
 dataframe_all = dataframe_all.ix[:,7:]
 # the list of columns (the last column is the class label)
 columns = dataframe_all.columns
-print columns
+print(columns)
+
+# get a sample to run test the algo on
+dataframe_all = dataframe_all.sample(frac=0.1)
 
 # step 3: get features (x) and scale the features
 # get x and convert it to numpy array
@@ -39,21 +41,17 @@ class_labels = np.unique(y)
 label_encoder = LabelEncoder()
 y = label_encoder.fit_transform(y)
 
-# step 5: split the data into training set and test set
-test_percentage = 0.1
-x_train, x_test, y_train, y_test = train_test_split(x_std, y, test_size = test_percentage, random_state = 0)
-
 # t-distributed Stochastic Neighbor Embedding (t-SNE) visualization
 from sklearn.manifold import TSNE
 tsne = TSNE(n_components=2, random_state=0)
-x_test_2d = tsne.fit_transform(x_test)
+x_2d = tsne.fit_transform(x)
 
 # scatter plot the sample points among 5 classes
 markers=('s', 'd', 'o', '^', 'v')
 color_map = {0:'red', 1:'blue', 2:'lightgreen', 3:'purple', 4:'cyan'}
 plt.figure()
-for idx, cl in enumerate(np.unique(y_test)):
-    plt.scatter(x=x_test_2d[y_test==cl,0], y=x_test_2d[y_test==cl,1], c=color_map[idx], marker=markers[idx], label=cl)
+for idx, cl in enumerate(np.unique(y)):
+    plt.scatter(x=x_2d[y==cl,0], y=x_2d[y==cl,1], c=color_map[idx], marker=markers[idx], label=cl)
 plt.xlabel('X in t-SNE')
 plt.ylabel('Y in t-SNE')
 plt.legend(loc='upper left')
